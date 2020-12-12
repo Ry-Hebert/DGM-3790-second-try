907f6bd91a6cfd8e746f73905dcf35d61d945b54
Ryan <ryan@thecpafirm.com>
2020-12-12 13:44:28 -0700

    cards opening and expanding

--- src/components/FleetGrab.js
+++ src/components/FleetGrab.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import clsx from 'clsx'
 import {
     Grid, 
     Button,
@@ -8,27 +9,38 @@ import {
     CardContent,
     CardActions,
     Typography,
-    Link
+    Link,
+    Collapse,
+    IconButton
 } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import '../assets/scss/fleetGrab.scss'
 import { useShipListContext } from '../contexts/ShipListContext'
-// import FleetData from '../assets/fleetyard'
+
+const useStyles = makeStyles((theme) => ({
+      expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest,
+        }),
+      },
+      expandOpen: {
+        transform: 'rotate(180deg)',
+      },
+}));
 
 const FleetGrab = () =>{
     console.log('Test')
     const manifest = useShipListContext()
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
 
-    // const [manifest, setManifest] = useState({
-    //     loading: false,
-    //     shipList: []
-    // })
-
-    // useEffect(() => {
-    //     setManifest({
-    //         loading: false,
-    //         shipList: 
-    //     })
-    // }, [])
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+    }
 
     return(
         <Grid className='cardGrid'>
@@ -55,7 +67,37 @@ const FleetGrab = () =>{
                         <CardActions>
                             <Button><Link href={element.storeUrl} target='_blank' rel='noopener'>Learn More</Link></Button>
                             {element.brochure != null ? <Button><Link href={element.brochure} target='_blank' rel='noopener'>View Brochure</Link></Button> : null}
+                            <IconButton className={clsx(classes.expand, {[classes.expandOpen]: expanded,})} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
+                                <ExpandMoreIcon />
+                            </IconButton>
                         </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                            <Typography paragraph>Method:</Typography>
+                            <Typography paragraph>
+                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                                minutes.
+                            </Typography>
+                            <Typography paragraph>
+                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                            </Typography>
+                            <Typography paragraph>
+                                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                                again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                                minutes more. (Discard any mussels that don’t open.)
+                            </Typography>
+                            <Typography>
+                                Set aside off of the heat to let rest for 10 minutes, and then serve.
+                            </Typography>
+                            </CardContent>
+                        </Collapse>
                     </Card>
                 )
             })}
