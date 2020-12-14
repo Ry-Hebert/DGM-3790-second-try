edda3404d43fdb637e1834c4a1d42f7ed8bc4e30
Ryan <ryan@thecpafirm.com>
2020-12-14 13:09:53 -0700

    maybe

--- src/components/FleetGrab.js
+++ src/components/FleetGrab.js
@@ -14,10 +14,10 @@ import {
     IconButton
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import '../assets/scss/fleetGrab.scss'
-import { useShipListContext } from '../contexts/ShipListContext'
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import '../assets/scss/fleetGrab.scss';
+import { useShipListContext } from '../contexts/ShipListContext';
 
 const useStyles = makeStyles((theme) => ({
       expand: {
